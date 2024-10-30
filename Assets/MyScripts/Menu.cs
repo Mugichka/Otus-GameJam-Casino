d698a4ca5c3fd7f4940add3245b5321be11521a7
Marsel <marselp2@yandex.ru>
2024-10-30 20:52:25 +0700

    Update Menu.cs

--- Assets/MyScripts/Menu.cs
+++ Assets/MyScripts/Menu.cs
@@ -8,7 +8,7 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("Maks");
+    
     }
 
     // Update is called once per frame
